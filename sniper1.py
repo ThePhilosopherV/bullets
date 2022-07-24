06e1464d7a8171facce1ab375f10059cf0cc792e
ThePhilosopherV <42734412+ThePhilosopherV@users.noreply.github.com>
2022-07-24 08:48:17 +0100

    Add files via upload

--- sniper1.py
+++ sniper1.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
-import socket, threading,sys,ipaddress
+import socket, threading,sys,ipaddress,datetime
 
-
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+        
 def TCP_connect(ip, port, delay):
     #print(ip)
     TCPsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -14,30 +18,55 @@ def TCP_connect(ip, port, delay):
 
 
 
-def scan_ports(ips, delay,port):
-
-    threads = []        
-
-    for ip in ips:
-        t = threading.Thread(target=TCP_connect, args=(ip, port, delay))
-        threads.append(t)
-
+def scan_ports(iplist, delay,port):
     
-    for i in range(len(ips)):
-        threads[i].start()
-
+    for ips in iplist:
+        threads = []        
     
-    for i in range(len(ips)):
-        threads[i].join()
+        for ip in ips:
+            t = threading.Thread(target=TCP_connect, args=(ip, port, delay))
+            threads.append(t)
+    
+        
+        for i in range(len(ips)):
+            threads[i].start()
+    
+        
+        for i in range(len(ips)):
+            try:
+                threads[i].join()
+            except KeyboardInterrupt:
+                print()
+                quit('CTRL+C signal')
 
 
 
 def main():
     netip=sys.argv[1]
     port = int(sys.argv[2])
+    nthread = int(sys.argv[3])
+    
     ips = [str(ip) for ip in ipaddress.IPv4Network(netip)]
+    print('number of targets: '+str(len(ips)))
+    
+    iplist = list(chunks(ips,nthread))
+    
     delay = 2   
-    scan_ports(ips, delay,port)
+    scan_ports(iplist, delay,port)
 
-if __name__ == "__main__":
-    main()
+
+t1 = datetime.datetime.now().replace(microsecond=0)
+main()
+t2= datetime.datetime.now().replace(microsecond=0)
+
+tl=str(t2-t1).split(':')
+print("time elapsed: ",end='')
+if tl[0] == '0' and tl[1]=='00':
+    print(tl[2]+'s')
+elif tl[0] != '0':
+        print(tl[0]+'h'+ tl[1]+'m'+tl[2]+'s' )
+
+elif tl[1] != '00':
+        print(tl[1]+'m'+tl[2]+'s')
+
+    
