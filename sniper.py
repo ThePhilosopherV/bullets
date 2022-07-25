bd31cf166f20e28aa1f08271e5979fd66f014cad
ThePhilosopherV <42734412+ThePhilosopherV@users.noreply.github.com>
2022-07-25 22:29:31 +0100

    Add files via upload

--- sniper.py
+++ sniper.py
@@ -2,6 +2,48 @@
 
 import socket, threading,sys,ipaddress,datetime,os
 
+def countries():
+    mypath = "country-ip-blocks/ipv4/"
+    countries = next(os.walk(mypath), (None, None, []))[2]
+    print("Available countries: ")
+    for c in countries:
+        print(c+',',end='')
+    print('\nNumber of countries Available: '+str(len(countries)))
+    quit('(Country codes as described by ISO 3166-1 alpha-2 )')
+    
+    
+def geo(country):
+    mypath = "country-ip-blocks/ipv4/"
+    countris = next(os.walk(mypath), (None, None, []))[2]  # [] if no file
+    if country not in countris:
+        countries()
+        
+    file=mypath+country
+    with open(file) as f:
+        lines = f.read().splitlines()
+    
+    c=0
+    
+    for ip in lines:
+        c+=1
+        print('{'+ip+':'+str(c)+'}, ',end='')
+    
+    print("\nNumber of IP ranges: "+str(len(lines)))
+    while 1:
+        try:
+            cntnmbr = int(input("Input IPs range number: "))
+        except ValueError:
+            print("Not a number")
+            continue
+        if len(lines)>= cntnmbr>=1:
+            print("")
+            return lines[cntnmbr-1]
+        elif  cntnmbr<0 :
+            print('Number must be positive')
+        else:
+            print('Number should be between 1 and '+str(len(lines)))
+            
+
 def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
@@ -45,18 +87,30 @@ def scan_ports(iplist, delay,port):
                 quit('CTRL+C signal')
 help= """Usage:
 
-./sniper <networkAddress/CIDR> <Port> <NumberOfThreads> [browser]
+./sniper <networkAddress/CIDR>/<Country code> <Port> <NumberOfThreads> [browser]
+
+<Country code> : Country code represented in two alphabets,type ./sniper --countries for more info
 
 [browser] :browser name is optinal, aslo activates IPs browsing; make sure [browser] is callable from any directory.
 
 Example: ./sniper.py 196.217.254.0/24 8080 200 firefox """
 def main():
+    
     if len(sys.argv)==1:
         print(help)
         quit()
+        
+    if sys.argv[1]=='--countries':
+        countries() 
+        
     global webflag
-    webflag=0
-    netip=sys.argv[1]
+    webflag=0 
+    
+    if len(sys.argv[1])==2:
+        netip=geo(sys.argv[1])
+    
+    else:netip=sys.argv[1]
+
     port = int(sys.argv[2])
     nthread = int(sys.argv[3])
     try:
@@ -66,8 +120,12 @@ def main():
         webflag=1
     except:
         pass
-    
-    ips = [str(ip) for ip in ipaddress.IPv4Network(netip)]
+    try:
+        ips = [str(ip) for ip in ipaddress.IPv4Network(netip)]
+    except:
+        print("Wrong network IP or country code")
+        quit()
+    print('Network IP: '+netip)
     print('number of targets: '+str(len(ips))+'\n')
     
     iplist = list(chunks(ips,nthread))
@@ -80,9 +138,12 @@ t1 = datetime.datetime.now().replace(microsecond=0)
 main()
 t2= datetime.datetime.now().replace(microsecond=0)
 
+#displaying time and number of positive targets
+
 tl=str(t2-t1).split(':')
 print('\nNumber of positive targets: '+str(len(targets)))
 print("time elapsed: ",end='')
+
 if tl[0] == '0' and tl[1]=='00':
     print(tl[2]+'s')
 elif tl[0] != '0':
@@ -90,6 +151,7 @@ elif tl[0] != '0':
 
 elif tl[1] != '00':
         print(tl[1]+'m'+tl[2]+'s')
+#browsing IPs
 
 num = len(targets)
 c=0
